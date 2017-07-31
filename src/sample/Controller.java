f53305b64703ab51b9c42af906febd7be8678531
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2017-07-31 14:43:26 -0500

    Definiendo vista de calculadora

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,80 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.awt.event.ActionEvent;
+
+
 public class Controller {
+
+    private String expression = "";
+    @FXML
+    private Label label;
+
+
+    public void addNine(ActionEvent event) {
+        expression += "9";
+        label.setText(expression);
+    }
+
+    public void addEight(ActionEvent event) {
+        expression += "8";
+        label.setText(expression);
+    }
+
+    public void addSeven(ActionEvent event) {
+        expression += "7";
+        label.setText(expression);
+    }
+
+    public void addSix(ActionEvent event) {
+        expression += "6";
+        label.setText(expression);
+    }
+
+    public void addFive(ActionEvent event) {
+        expression += "5";
+        label.setText(expression);
+    }
+
+    public void addFour(ActionEvent event) {
+        expression += "4";
+        label.setText(expression);
+    }
+
+    public void addThree(ActionEvent event) {
+        expression += "3";
+        label.setText(expression);
+    }
+
+    public void addTwo(ActionEvent event) {
+        expression += "2";
+        label.setText(expression);
+    }
+
+    public void addOne(ActionEvent event) {
+        expression += "1";
+        label.setText(expression);
+    }
+
+    public void addDivition(ActionEvent event) {
+        expression += "/";
+        label.setText(expression);
+    }
+
+    public void addMultiplication(ActionEvent event) {
+        expression += "*";
+        label.setText(expression);
+    }
+
+    public void addSubtration(ActionEvent event) {
+        expression += "-";
+        label.setText(expression);
+    }
+
+    public void addSum(ActionEvent event) {
+        expression += "+";
+        label.setText(expression);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculator");
+        Scene scene = new Scene(root, 300, 300);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
